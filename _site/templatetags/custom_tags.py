9151e6d10891cc8f2617e8a0cafd9257b0577461
volkov.sinergium.ru <volkov@sinergium.ru>
2025-03-09 19:29:29 +0300

    Добавлена ленивая загрузка для изображений

--- _site/templatetags/custom_tags.py
+++ _site/templatetags/custom_tags.py
@@ -5,11 +5,12 @@ from django.templatetags.static import static
 register = template.Library()
 
 @register.simple_tag
-def img_tag(src, width, height, classes="", alt="", is_absolute_src=False):
+def img_tag(src, width, height, classes="", alt="", is_absolute_src=False, is_lazy=False):
     static_src = static('/modules/' + str(src))
+    is_lazy = 'lazy' if is_lazy else 'eager'
     
     if is_absolute_src:
         static_src = src
 
-    html = f'<img src="{static_src}" width="{width}" height="{height}" class="{classes}" alt="{alt}" onError="this.src = `https://placehold.co/{width}x{height}`">'
+    html = f'<img src="{static_src}" width="{width}" height="{height}" class="{classes}" alt="{alt}" loading="{is_lazy}" onError="this.src = `https://placehold.co/{width}x{height}`">'
     return mark_safe(html)
